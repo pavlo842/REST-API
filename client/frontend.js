9dfe99b29d27ee547eb61f52213b787c3a1878a0
pavel <pishchalaups@belhard.com>
2020-05-07 21:24:46 +0300

    form

--- client/frontend.js
+++ client/frontend.js
@@ -1,4 +1,37 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
-console.log(Vue);
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: [
+                {id: 1, name: 'Pallo', value: '+37529 694-63-84', marked: false}
+            ],
+        }
+    },
+    computed: {
+        canCreate() {
+            return this.form.value.trim() && this.form.name.trim();
+        }
+    },
+    methods: {
+        createContact() {
+            const {...contact} = this.form;
 
+            this.contacts.push({...contact, id: Date.now(), marked: false});
+            
+            this.form.name = this.form.value = '';
+        },
+        markContact(id) {
+            const contact = this.contacts.find(c => c.id === id);
+            contact.marked = true;
+        },
+        removeContact(id) {
+
+        }
+    }
+});
