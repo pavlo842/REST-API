f2ba68659bcd7cc485e45c2e93195b1935e9f93f
pavel <pishchalaups@belhard.com>
2020-05-08 11:11:08 +0300

    add get, post, delete, put

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { v4 } = require('uuid'); // рлдключение библиотеки для генерации id
 
+// Импровизированная БД
+let CONTACTS = [
+    {id: v4(), name: 'Pallo', value: '+37529 694-63-84', marked: false} // v4() - вызов функции для генерации id
+]
 
+app.use(express.json()); // добавлена возможность работать с json
+
+// GET первый url по которому получаются данные
+app.get('/api/contacts', (req, res) => {
+    setTimeout(() => {
+        res.status(200).json(CONTACTS)
+    }, 1000); // интервал для того чтобы показать спинер - имитация загрузки
+})
+
+// POST Создание данных
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact);
+    res.status(201).json(contact); // 201 - элемент создан
+})
+
+// DELETE Удаление данных
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+    res.status(200).json({message: 'Contact was deleted!'})
+})
+
+// PUT модификация данных
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id);
+    CONTACTS[idx] = req.body;
+    res.json(CONTACTS[idx]);
+});
 
 // Далее код должен храниться внизу - отрабатываться последним
 app.use(express.static(path.resolve(__dirname, 'client')));
--- client/frontend.js
+++ client/frontend.js
@@ -1,16 +1,26 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
+// Создание компонента со спинером
+Vue.component('loader', {
+    template: `
+        <div style="display: flex; justify-content: center; align-items: center;">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    `
+})
+
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: false, // Для спинера
             form: {
                 name: '',
                 value: ''
             },
-            contacts: [
-                {id: 1, name: 'Pallo', value: '+37529 694-63-84', marked: false}
-            ],
+            contacts: [],
         }
     },
     computed: {
@@ -19,19 +29,58 @@ new Vue({
         }
     },
     methods: {
-        createContact() {
+        async createContact() { // для создания делается async
             const {...contact} = this.form;
 
-            this.contacts.push({...contact, id: Date.now(), marked: false});
+            // далее запрос на создание
+            const newContact = await request('/api/contacts', 'POST', contact);
+            
+            this.contacts.push(newContact); // получен newContact и запушен в массив contacts
             
             this.form.name = this.form.value = '';
         },
-        markContact(id) {
+        async markContact(id) {
             const contact = this.contacts.find(c => c.id === id);
-            contact.marked = true;
+            const upd = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact, // spread
+                marked: true,
+            });
+            contact.marked = upd.marked;
         },
-        removeContact(id) {
-            this.contacts = this.contacts.filter(c => c.id !== id); // удаление элемента
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE'); // Удаление на ВЕ
+            this.contacts = this.contacts.filter(c => c.id !== id); // Если на ВЕ удалено, то удаление элемента на FE
         }
+    },
+    // метод mounted() - вызывается когда компонент готов
+    async mounted() {
+        // console.log('Ready');
+        this.loading = true;
+        this.contacts = await request('/api/contacts'); // получение данных с ВЕ и запись их в this.contacts
+                                                        // если не один и тот же порт то нужно указывать 'http://localhost:3000/.......'
+        this.loading = false;
     }
 });
+
+// Функция для запроса к серверу
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {};
+        let body;
+
+        if (data) {
+            headers['Content-Type'] = 'application/json';
+            body = JSON.stringify(data);
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        });
+        return await response.json();
+    } catch (e) {
+        console.warn('Error', e.message);        
+    }
+}
